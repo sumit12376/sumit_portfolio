9cd1fe70d8566fc591e3e3c03b9f5c9590c2fdf6
Sumit vikram singh <sumitvikramsingh46@gmail.com>
2024-08-17 23:10:55 +0530

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,26 +11,18 @@ const Footer = () => {
         <p className="text-gray-400">
           &copy;  Sumit. All rights reserved.
         </p>
-        <div className="flex space-x-4 my-4 md:my-0">
-          <a href="https://www.linkedin.com/in/sumit-vikram-singh-936310225/" className="text-gray-400 hover:text-white">
-            <FaLinkedin />
+        <div className="flex space-x-4 my-4 md:my-0 gap-4">
+          <a href="https://www.linkedin.com/in/sumit-vikram-singh-936310225/" target="_blank" className="text-gray-400 hover:text-white">
+            <FaLinkedin className="w-9 h-9"/>
           </a>
-          <a href="https://github.com/sumit12376" className="text-gray-400 hover:text-white">
-            <FaGithub />
+          <a href="https://github.com/sumit12376" target="_blank" className="text-gray-400  hover:text-white">
+            <FaGithub  className="w-9 h-9"/>
           </a>
-          <a href="" className="text-gray-400 hover:text-white">
-            <FaFacebook />
+          <a  href="https://www.facebook.com/profile.php?id=100070373857062" target="_blank" className="text-gray-400 hover:text-white ">
+            <FaFacebook className="w-9 h-9" />
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
-            <FaInstagram />
-          </a>
-        </div>
-        <div className="flex space-x-4">
-          <a href="#" className="text-gray-400 hover:text-white">
-            Privacy
-          </a>
-          <a href="#" className="text-gray-400 hover:text-white">
-            Terms of Service
+          <a href="https://www.instagram.com/sumit.thakurr_1/" target="_blank" className="text-gray-400 hover:text-white">
+            <FaInstagram className="w-9 h-9 mr-6" />
           </a>
         </div>
       </div>
