37c303b3d52de8d22603d755ba0c52d799aaa7fa
sumit v singh <sumitvikramsingh36@gmail.com>
2024-09-13 18:52:23 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import HeroImage from '../assets/hero-image.png';
 import Header from './Header';
 const Hero = () => {
   const handleResumeClick = () => {
-    window.open('https://drive.google.com/file/d/1GRCC2xVfKvdK_FIuiViPujOhrw5wrlju/view?usp=sharing', '_blank');
+    window.open('https://drive.google.com/file/d/1wQY2dLANikDXOIL-novs_2-UeCfxD_va/view?usp=sharing', '_blank');
   };
 
   return (
