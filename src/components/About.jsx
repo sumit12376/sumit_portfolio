c37fc20f602b3337f8998d2d58e9b24384665647
sumit v singh <sumitvikramsingh36@gmail.com>
2024-10-31 19:16:22 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import AboutImage from "../assets/hero-image.png";
 
 const About = () => {
   return (
-    <div className="bg-black text-white py-20" id="about">
+    <div className="bg-black text-white py-20"  id="about">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
