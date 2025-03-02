2de597826a55c17b53e327accfd4bd9978355b95
sumit v singh <sumitvikramsingh36@gmail.com>
2025-03-02 21:31:03 +0530

    change-resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
 
   const handleResumeClick = () => {
     window.open(
-      "https://drive.google.com/file/d/1VzTyJrP44Ttqp45dGglj651bSXwje63X/view?usp=sharing",
+      "https://drive.google.com/file/d/1eqraDj4sMiVQ0GjCkqUPzrkcVprui8wn/view?usp=sharing",
       "_blank"
     );
   };
